be8cc9cc2dc6bcb79131fb8659f26846a856ab3f
Devonte M <devontem@live.com>
2016-03-09 17:41:03 -0800

    added routes for portfolio and rooms

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -6,6 +6,12 @@ var http = require('http-request');
 
 module.exports = function (app, express) {
 
+  // Creating routers
+  var userRouter = express.Router();
+  var roomRouter = express.Router();
+  var portfolioRouter = express.Router();
+
+  // Configuring middleware
   app.use(bodyParser.urlencoded({ extend: true }));
   app.use(bodyParser.json());
   app.use(cookieParser());
@@ -14,12 +20,22 @@ module.exports = function (app, express) {
   app.use(passport.session());
   app.use(express.static(__dirname + '/../../client'));
 
+  // test for sample query
   http.get('http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22AAPL%22)&env=store://datatables.org/alltableswithkeys&format=json', function(err, res){
     if(err){
       console.error(err);
       return
     }
     console.log(res.buffer.toString());
-  })
+  });
+
+  // Connecting Router to route files
+  app.use('/api/users', userRouter);
+  app.use('/api/rooms', roomRouter);
+  app.use('/api/portfolo', portfolioRouter);
+
+  require('../users/userRoutes.js')(userRouter);
+  require('../rooms/roomRoutes.js')(roomRouter);
+  require('../portfolios/portfolioRoutes.js')(portfolioRouter);
 
 }
