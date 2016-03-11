67fc7c7185a37e39e175dc085742b435e7e96714
jordmur <jordanmurphy11@gmail.com>
2016-03-10 19:04:50 -0600

    (feat) Adds dummy data to the leagues

--- client/profile/profileController.js
+++ client/profile/profileController.js
@@ -1,6 +1,18 @@
 angular.module('app.profile', [])
 
 .controller('ProfileController', ['$scope', function ($scope) {
-  $scope.leagues = [];
-  
+
+  $scope.leagues = [{name:"league1", userRank: 2, percentChange: -1.3, gainLoss: '123.00', totalValue: 14000, changeYesterday: 2.5}, {name:"league2", userRank: 17, percentChange: 15, gainLoss: '2000', totalValue: 9888, changeYesterday: 2.5}, {name:"league3", userRank: 1, percentChange: 2.5, gainLoss: '12.00', totalValue: 9012, changeYesterday: 2.5}];
+  $scope.portfolios = [];
+  //Setting up some basic dummy data for the leagues
+
+
+  $scope.getProfileLeagues = function () {
+    // TODO: this will connect to a factory to pull leagues
+  }
+
+  $scope.getProfilePortfolios = function () {
+    // TODO: this will connect to a factory to pull user portfolios
+  }
+
 }])
