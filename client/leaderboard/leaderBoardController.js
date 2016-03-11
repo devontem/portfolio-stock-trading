a7d93c514a7cc03868ac4f1355b004da2f53c0aa
Sonny Sheth <sonny.sheth@gmail.com>
2016-03-11 16:54:47 -0300

    (add) setup initial methods to get leaderboard data and to update

--- client/leaderboard/leaderBoardController.js
+++ client/leaderboard/leaderBoardController.js
@@ -1,5 +1,17 @@
-app.controller('LeaderBoardController', function(){
+app.controller('LeaderBoardController', function($scope){
 
+  // members will be an object of each member in the league
+  // containing name, portfolio value, and other stats
+  // desired to go on the leaderboard
+  $scope.members;
+
+  $scope.getLeaderBoard = function(){
+    // this will call a factory function to grab http data from server and assign returned data to $scope.members;
+  };
+
+  $scope.updateLeaderBoard = function(){
+    // this will call a factory function to update the leaderboard, e.g., updating ranking, stats, as well reflecting a change when a member leaves the league;
+  }
 
 
 });
