8e32c7003b239e21cf7a0fbcde454cc5a1d2b5b2
Sonny Sheth <sonny.sheth@gmail.com>
2016-03-11 19:53:34 -0300

    (add)add ng-repeat to leaderboard.html

--- client/leaderboard/leaderBoardController.js
+++ client/leaderboard/leaderBoardController.js
@@ -4,12 +4,16 @@ app.controller('LeaderBoardController', function($scope, leaderBoardService){
   // containing name, portfolio value, and other stats
   // desired to go on the leaderboard
   $scope.members;
+  $scope.leagueId;
 
-  $scope.getLeaderBoard = function(leagueID){
+  $scope.getLeaderBoard = function(leagueId){
     // this will call a factory function to grab http data from server and assign returned data to $scope.members;
-    leaderBoardService.getMembers(leagueID).then(function(members){
+    leaderBoardService.getMembers(leagueId).then(function(members){
       $scope.members = members;
     })
   };
+
+  // once we have league ID, call to initialize leaderboard
+  //$scope.getLeaderBoard(leagueId);
 
 });
