3f9cbe34e778d1700d12c3243bffb02f763a6821
jordmur <jordanmurphy11@gmail.com>
2016-03-11 16:21:16 -0600

    (feat) Can now toggle between views

--- client/profile/profileController.js
+++ client/profile/profileController.js
@@ -5,7 +5,16 @@ angular.module('app.profile', [])
   $scope.userLeagues = [{name:"league1", userRank: 2, percentChange: -1.3, gainLoss: '123.00', totalValue: 14000, changeYesterday: 2.5}, {name:"league2", userRank: 17, percentChange: 15, gainLoss: '2000', totalValue: 9888, changeYesterday: 2.5}, {name:"league3", userRank: 1, percentChange: 2.5, gainLoss: '12.00', totalValue: 9012, changeYesterday: 2.5}];
   $scope.leaguesToJoin = [{name:"league4", userRank: 2, percentChange: -1.3, gainLoss: '123.00', totalValue: 14000, changeYesterday: 2.5}, {name:"league5", userRank: 17, percentChange: 15, gainLoss: '2000', totalValue: 9888, changeYesterday: 2.5}, {name:"league6", userRank: 1, percentChange: 2.5, gainLoss: '12.00', totalValue: 9012, changeYesterday: 2.5}];
 
-  $scope.currentLeagueView = ''
+  $scope.currentTab = 'user';
+
+  $scope.showToJoin = function () {
+    $scope.currentTab = 'toJoin';
+
+  }
+
+  $scope.showUserLeagues = function () {
+    $scope.currentTab = 'user';
+  }
 
   $scope.getUserLeagues = function () {
     // TODO: this will connect to a factory to pull leagues user is in
