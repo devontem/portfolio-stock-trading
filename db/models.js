393ae43b95a920bf5b681b2db6f3366ed0f7e2c5
Sonny Sheth <sonny.sheth@gmail.com>
2016-03-12 16:43:32 -0300

    (refactor) add userId to Messages model

--- db/models.js
+++ db/models.js
@@ -59,6 +59,7 @@ var Transaction = orm.define('Transaction', {
 //Message Board Model
 
 var Message = orm.define('Message', {
+  userId: Sequelize.INTEGER,
   name: Sequelize.STRING,
   message: Sequelize.STRING
 })
@@ -68,7 +69,7 @@ var Message = orm.define('Message', {
 var League = orm.define('league', {
 	name: Sequelize.STRING,
   maxNum: Sequelize.INTEGER,
-  startbalance: Sequelize.INTEGER 
+  startbalance: Sequelize.INTEGER
 });
 
 //Joint table for League and user
