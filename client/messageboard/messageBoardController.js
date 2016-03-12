97d1e605504424e4629a26e7c116ff954b3be1e3
Sonny Sheth <sonny.sheth@gmail.com>
2016-03-12 02:57:10 -0300

    Merge conflict

--- client/messageboard/messageBoardController.js
+++ client/messageboard/messageBoardController.js
@@ -1,5 +1,30 @@
 app.controller('MessageBoardController', function($scope){
 
+  $scope.posts = [
+    {
+      username: 'Sonny',
+      message: 'hey hey hey'
+    },
+    {
+      username: 'Jordan',
+      message: 'Not much ...'
+    },
+    {
+      username: 'Jonathon',
+      message: 'Word to Big Bird'
+    }
+  ];
 
+  $scope.leagueId;
+
+  $scope.getMessageBoard = function(leagueId){
+    // this will call a factory function to grab http data from server and assign returned data to $scope.members;
+    messageBoardFactory.getPosts(leagueId).then(function(posts){
+      $scope.posts = posts;
+    })
+  };
+
+  // once we have league ID, call to initialize leaderboard
+  //$scope.getLeaderBoard(leagueId);
 
 });
