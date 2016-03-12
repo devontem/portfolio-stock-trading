33f68c84e51fd1837117b59a7f7bdba7905f51f3
Sonny Sheth <sonny.sheth@gmail.com>
2016-03-12 20:53:18 -0300

    commit to rebase

--- server/messageboard/messageController.js
+++ server/messageboard/messageController.js
@@ -3,13 +3,14 @@ var Message = require('../../db/models').Message;
 module.exports.addPostToDB = function (req, res){
 
   Message.create({
+    leagueId: req.body.leagueId,
     userId: req.body.userId,
     name: req.body.name,
     message: req.body.message
   })
   .then(function (post) {
     console.log('======', post)
-    res.send({name: post.name, message: post.message})
+    res.send({leagueId: post.leagueId, name: post.name, message: post.message})
   })
   .catch(function (err) {
     console.error('Error creating message entry: ', err.message);
