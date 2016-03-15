ca12df76347054e4d2427ccf63be66be2117858a
jordmur <jordanmurphy11@gmail.com>
2016-03-14 21:43:04 -0500

    (feat) Correctly tests if start is later than current moment

--- client/league/leagueController.js
+++ client/league/leagueController.js
@@ -2,20 +2,28 @@ app.controller('LeagueController', ['$scope', '$stateParams', 'DashboardFactory'
   //Gets the league
   var leagueId = $stateParams.leagueId;
 
-  DashboardFactory.getLeagueById(leagueId)
-    .then(function(league){
-      console.log('started');
-      $scope.league = league;
-      console.log('************************',$scope.league);
-    });
-
   // initialize a flag indicating that the league has not started yet
   $scope.hasStarted = false;
   // grab the current moment using moment.js
-  var currentMoment = moment();
+  var currentMoment = moment().utc();
 
-  var checkStart = function () {
-    console.log('************************',$scope.league);
+  $scope.checkStart = function (league) {
+    var start = moment(league.start).utc();
+    console.log('&23948723984723987', start);
+    console.log('))))))))))))))', currentMoment);
+    console.log(currentMoment.isValid());
+    if (currentMoment.isBefore(start)) {
+      console.log('&&&&&&&&&&&&&&&&&&&');
+    } else {
+      console.log('doink');
+    }
   };
-  // checkStart();
+
+  DashboardFactory.getLeagueById(leagueId)
+    .then(function(league){
+      $scope.league = league;
+      $scope.checkStart($scope.league);
+    });
+
+
 }]);
