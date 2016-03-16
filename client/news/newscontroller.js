4458fa6efa48972a32d1a0758de4a63825d2afe9
Devonte M <devontem@live.com>
2016-03-16 15:42:55 -0700

    added some styling changes

--- client/news/newscontroller.js
+++ client/news/newscontroller.js
@@ -5,32 +5,27 @@ app.controller('NewsController', function($scope, $window, $stateParams, News){
 
   $scope.getTweets= function(){
 
-  	var leagueId = $stateParams.leagueId;
-	var userId = $window.localStorage.getItem('com.tp.userId');
-  	$scope.tweets = [];
-  	News.getNews(userId, leagueId)
-  	.then(function (res){
-  		res.data.forEach(function(tweet){
-  			$scope.tweets.push({text : tweet.text, user : tweet.user, time: tweet.created_at})
-  		})
-  		
-  	})
+    var leagueId = $stateParams.leagueId;
+  var userId = $window.localStorage.getItem('com.tp.userId');
+    $scope.tweets = [];
+    News.getNews(userId, leagueId)
+    .then(function (res){
+      res.data.forEach(function(tweet){
+        $scope.tweets.push({text : tweet.text, user : tweet.user, time: tweet.created_at})
+      })
+      
+    })
   }
   $scope.getTweets();
 })
 .factory('News', function($http) {
-	var getNews = function(userId, leagueId) {
-		return $http({
-			method: 'Get',
-			url: '/api/tweets/'+leagueId+'/'+userId
-		}).then(function(data){
-      console.log('data', data)
+  var getNews = function(userId, leagueId) {
+    return $http({
+      method: 'Get',
+      url: '/api/tweets/'+leagueId+'/'+userId
     })
-	}
-	return {
-		getNews: getNews
-	}
+  }
+  return {
+    getNews: getNews
+  }
 })
-
-
-
