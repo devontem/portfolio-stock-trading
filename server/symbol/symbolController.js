66261d79fbd99f7d0d34d016e55575ece649460a
Jonathon Lo <jonathon.lo35@gmail.com>
2016-03-16 11:27:26 -0700

    (fix) filter stock symbols and add toasts

--- server/symbol/symbolController.js
+++ server/symbol/symbolController.js
@@ -5,11 +5,13 @@ module.exports.getSymbol = function(req, res){
   var company = req.params.company;
   var query = "http://d.yimg.com/aq/autoc?query=" + company + "&region=US&lang=en-US";
 
+//
+
   request(query, function(err, result){
     if (err) {
       throw err;
     }
-
+    
     res.send(result.body);
   })
 
