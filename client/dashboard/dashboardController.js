1af57305f12e237ec7dcbd9628b607cde293bc53
abc810221 <abc810221@gmail.com>
2016-03-17 13:07:27 -0700

    materialized datepicker

--- client/dashboard/dashboardController.js
+++ client/dashboard/dashboardController.js
@@ -43,6 +43,52 @@ angular.module('app.dashboard', [])
   $scope.portfolios = {};
   $scope.numtojoin = 0;
 
+  $scope.pickstart = function(){
+    var start = $('#startdate').pickadate({
+      onSet: function (context) {
+        $scope.league.start = new Date(context.select);
+      },
+      onClose: function() {
+          $('#startdate').focus();
+      },
+      selectMonths: true,
+      selectYears: 15,
+      editable: true
+    });
+    var picker = start.pickadate('picker');
+    start.on('click', function(event) {
+        if (picker.get('open')) {
+            picker.close();
+        } else {
+            picker.open();
+        }                        
+        event.stopPropagation();
+    });
+  }
+
+  $scope.pickend = function(){
+    var end = $('#enddate').pickadate({
+      onSet: function (context) {
+        $scope.league.end = new Date(context.select)
+      },
+      onClose: function() {
+          $('#enddate').focus();
+      },
+      selectMonths: true,
+      selectYears: 15,
+      editable: true
+    });
+    var pickers = end.pickadate('picker');
+    end.on('click', function(event) {
+        if (pickers.get('open')) {
+            pickers.close();
+        } else {
+            pickers.open();
+        }                        
+        event.stopPropagation();
+    });
+  }
+
   //toggle add form
   $scope.showadd = false;
   $scope.toggleAdd = function(){
