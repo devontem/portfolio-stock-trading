e3c95b993364f7ec94f74f31235f07e3dcd1b305
jordmur <jordanmurphy11@gmail.com>
2016-03-18 12:52:20 -0500

    (feat) Loops over available badges

--- client/badges/badgeController.js
+++ client/badges/badgeController.js
@@ -1,3 +1,4 @@
 app.controller('BadgeController', ['$scope', function($scope){
   $scope.test = 100;
+  $scope.badges = ['Sign In', 'Joined First League', 'Won First League', 'Made Your First Mil'];
 }]);
