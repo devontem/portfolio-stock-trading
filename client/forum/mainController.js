52be9ad3d6c968b0d2a8e8aebfc874171b06d34d
Sonny Sheth <sonny.sheth@gmail.com>
2016-03-18 18:14:28 -0300

    (add) create new topic form displays data to console

--- client/forum/mainController.js
+++ client/forum/mainController.js
@@ -1,9 +1,16 @@
 var app = angular.module('app')
 
-app.controller('MainForumController', ['$scope', function($scope){
+app.controller('MainForumController', ['$scope', '$window', function($scope, $window){
+
+  $scope.topic = {};
+  $scope.topic.username = $window.localStorage.getItem('com.tp.username');
 
   $scope.openModal = function(){
     $('#createForumPost').openModal();
+  }
+
+  $scope.createTopic = function(topic){
+    console.log('NEW TOPIC:', topic)
   }
 
 }])
