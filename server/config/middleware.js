e634e8e57e7c068d2ec1d4a7edd41873abdc63aa
abc810221 <abc810221@gmail.com>
2016-03-20 11:25:53 -0700

    Moved limit order function to seperate file

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -15,7 +15,7 @@ var Topic = require('../../db/models').Topic;
 var expressJwt = require('express-jwt');
 var jwt = require('jsonwebtoken');
 var morgan = require('morgan');
-//var processTrade = require('../transactions/transactionController').buySell;
+var limitOrder = require('./limitOrder').limitOrder;
 
 
 module.exports = function (app, express) {
@@ -44,84 +44,11 @@ module.exports = function (app, express) {
   app.use(passport.session());
   app.use(express.static(__dirname + '/../../client'));
 
+  limitOrder();
+
   // http.get('http://ichart.finance.yahoo.com/table.csv?s=AAPL&a=01&b=01&c=2014&d=01&e=01&f=2015&g=d&ignore=.csv', function(err, res){
   //   console.log(res.buffer.toString(),' ######')
   // })
-
-  setInterval(function(){
-    Order.findAll({ where: { executed: false }})
-      .then(function(orders){
-        var list = ' ';
-        for(var i=0; i<orders.length; i++){
-          list+=orders[i].dataValues.symbol + '+';
-        }
-        http.get('http://finance.yahoo.com/d/quotes.csv?s=' + list + '&f=sa', function(err, res){
-          var ask = res.buffer.toString().split('\n');
-          for(var i=0; i<orders.length; i++){
-            //if set price is greater than current price, process trade
-            if(Number(ask[i][1].split(',')[1]) < orders[i].dataValues.price){
-              Order.destroy({ where: {id: orders[i].id }})
-              var trade = {
-                 symbol: orders[i].dataValues.symbol,
-                 company: orders[i].dataValues.company,
-                 portfolioId: orders[i].dataValues.PortfolioId,
-                 shares: orders[i].dataValues.shares,
-                 price: orders[i].dataValues.price,
-                 marketPrice: orders[i].dataValues.price,
-                 buysell: orders[i].dataValues.buysell,
-                 executed: true
-              }
-              placeTrade(trade);
-              placeOrder(trade);
-            }
-          }
-        })
-      })
-  }, 4000)
-
-  function placeOrder (trade){
-    trade.PortfolioId = trade.portfolioId;
-    Order.create(trade);
-  }
-
-  function placeTrade (trade){
-    Transaction.create(trade).then(function(transaction){
-
-      Portfolio.findOne({ where: { id: trade.portfolioId }})
-      .then(function(portfolio){
-
-        if (!transaction.buysell) { transaction.shares = -1 * transaction.shares; }
-
-        var amount = transaction.price*transaction.shares;
-
-        //amount is either subtracted or added to portfolioValue depending on if sold or bought
-        portfolio.portfolioValue += amount;
-        portfolio.balance -= (amount+10);
-
-
-        //Setting the transaction's PortfolioId
-        transaction.PortfolioId = portfolio.id;
-
-        //Incrementing number of trades
-        portfolio.numOfTrades++;
-
-        //Setting the initial return
-        transaction.return = 0;
-
-        // Saving both instances
-        transaction.save();
-        portfolio.save();
-
-      })
-      .catch(function(err){
-        console.log("ERRORERROR")
-      });
-
-    })
-    .catch(function(err){
-        console.log("ERRORERROR")
-    });
-  }
 
   // Connecting Router to route files
   app.use('/api/users', userRouter);
