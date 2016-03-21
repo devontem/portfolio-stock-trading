ef0a09767279cd39fce9387519e006346709bff0
jordmur <jordanmurphy11@gmail.com>
2016-03-21 14:00:09 -0500

    (style) Moves news factory to services

--- client/news/newscontroller.js
+++ client/news/newscontroller.js
@@ -1,31 +1,20 @@
-app.controller('NewsController', function($scope, $window, $stateParams, News){
+app.controller('NewsController', ['$scope', '$window', '$stateParams', 'News', function($scope, $window, $stateParams, News){
 
-  
+
   $scope.tweets = [];
 
   $scope.getTweets= function(){
 
     var leagueId = $stateParams.leagueId;
-  var userId = $window.localStorage.getItem('com.tp.userId');
+    var userId = $window.localStorage.getItem('com.tp.userId');
     $scope.tweets = [];
     News.getNews(userId, leagueId)
     .then(function (res){
       res.data.forEach(function(tweet){
-        $scope.tweets.push({text : tweet.text, user : tweet.user, time: tweet.created_at})
-      })
-      
-    })
-  }
+        $scope.tweets.push({text : tweet.text, user : tweet.user, time: tweet.created_at});
+      });
+
+    });
+  };
   //$scope.getTweets();
-})
-.factory('News', function($http) {
-  var getNews = function(userId, leagueId) {
-    return $http({
-      method: 'Get',
-      url: '/api/tweets/'+leagueId+'/'+userId
-    })
-  }
-  return {
-    getNews: getNews
-  }
-})
+}]);
