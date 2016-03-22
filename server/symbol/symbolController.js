3cd02d2285c3231ceed4e8dc947332d76614c700
Jonathon Lo <jonathon.lo35@gmail.com>
2016-03-21 20:14:22 -0700

    (feat) Add watchlist

--- server/symbol/symbolController.js
+++ server/symbol/symbolController.js
@@ -1,4 +1,8 @@
 var request = require('request');
+var http = require('http-request');
+var Portfolio = require('../../db/models').Portfolio;
+var Transaction = require('../../db/models').Transaction;
+var Watchlist = require('../../db/models').Watchlist;
 
 module.exports.getSymbol = function(req, res){
 
@@ -15,4 +19,43 @@ module.exports.getSymbol = function(req, res){
     res.send(result.body);
   })
 
+}
+
+
+module.exports.limitOrder = function(){
+  setInterval(function(){
+    Order.findAll({ where: { executed: false }})
+      .then(function(orders){
+        var list = ' ';
+        for(var i=0; i<orders.length; i++){
+          list+=orders[i].dataValues.symbol + '+';
+        }
+        http.get('http://finance.yahoo.com/d/quotes.csv?s=' + list + '&f=sa', function(err, res){
+          var ask = res.buffer.toString().split('\n');
+          for(var i=0; i<orders.length; i++){
+            //if set price is greater than current price, process trade
+            if(Number(ask[i][1].split(',')[1]) < orders[i].dataValues.price){
+              Order.destroy({ where: {id: orders[i].id }})
+              var trade = {
+                 symbol: orders[i].dataValues.symbol,
+                 company: orders[i].dataValues.company,
+                 portfolioId: orders[i].dataValues.PortfolioId,
+                 shares: orders[i].dataValues.shares,
+                 price: orders[i].dataValues.price,
+                 marketPrice: orders[i].dataValues.price,
+                 buysell: orders[i].dataValues.buysell,
+                 executed: true
+              }
+              placeTrade(trade);
+              placeOrder(trade);
+            }
+          }
+        })
+      })
+  }, 4000)
+}
+
+function placeOrder (trade){
+  trade.PortfolioId = trade.portfolioId;
+  Order.create(trade);
 }
