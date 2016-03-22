19377b8653c4584dc3c6dfce4ec43d64bc6f7358
jordmur <jordanmurphy11@gmail.com>
2016-03-22 15:57:10 -0500

    (style) Updates for consistency

--- server/portfolios/portfolioRoutes.js
+++ server/portfolios/portfolioRoutes.js
@@ -2,7 +2,7 @@ var portfolioController = require('./portfolioController.js');
 var Auth = require('./../config/auth.js');
 
 module.exports = function(app){
-  
+
   // app.post('/', portfolioController.addPortfolioToDB);
 
   // app.put('/:id', portfolioController.updatePortfolio);
@@ -13,7 +13,7 @@ module.exports = function(app){
 
   app.get('/stocks/:leagueId/:userId', Auth.authorize, portfolioController.getUserStocks);
   app.put('/stocks/:leagueId/:userId', Auth.authorize, portfolioController.updateUserStocks);
-  
+
   // gets users portfolio basic info
   app.get('/:leagueId/:userId', Auth.authorize, portfolioController.getPortfolio);
-}
+};
