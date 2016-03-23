2c7199aa2168742ac360785979bafc92a149c890
Sonny Sheth <sonny.sheth@gmail.com>
2016-03-22 23:59:48 -0300

    Add league start date to no trading message

--- client/preleague/preleagueController.js
+++ client/preleague/preleagueController.js
@@ -1,4 +1,4 @@
-app.controller('PreLeagueController', ['$scope', '$stateParams', 'preLeagueFactory', function($scope, $stateParams, preLeagueFactory){
+app.controller('PreLeagueController', ['$scope', '$stateParams', 'preLeagueFactory', 'DashboardFactory', function($scope, $stateParams, preLeagueFactory, DashboardFactory){
 
   $scope.leagueId = $stateParams.leagueId;
   $scope.leagueName;
@@ -6,17 +6,29 @@ app.controller('PreLeagueController', ['$scope', '$stateParams', 'preLeagueFacto
   $scope.getLeagueName = function(){
     preLeagueFactory.getName($scope.leagueId)
       .then(function(name){
+        console.log('NAME???', name)
         $scope.leagueName = name;
       });
   };
 
-  $scope.pre = false;
-  $scope.suspended = true;
+  $scope.pre = true;
+  $scope.suspended = false;
 
   $scope.getLeagueName();
 
   // this is the start date, time of the league
   $scope.date;
 
+  $scope.leagueInfo;
+
+  $scope.getLeagueById = function(){
+    DashboardFactory.getLeagueById($scope.leagueId).then(function(data){
+      console.log('DATATATATAT', data)
+      $scope.leagueInfo = data;
+    });
+  };
+
+  $scope.getLeagueName();
+  $scope.getLeagueById();
 
 }]);
