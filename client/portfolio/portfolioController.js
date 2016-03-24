b3059b25380c11f44e169af0f1513e4f1766f192
Sonny Sheth <sonny.sheth@gmail.com>
2016-03-24 13:37:30 -0300

    (fix) portfolio val and % now correctly updates on buy or sell trxn

--- client/portfolio/portfolioController.js
+++ client/portfolio/portfolioController.js
@@ -78,11 +78,16 @@ angular.module('app.portfolio', [])
 				Materialize.toast('You traded '+options.shares+' shares in '+options.company, 3000, 'rounded');
 				$scope.resetFields();
 
+			}).then(function(){
+				$rootScope.$emit('bought');
+				$rootScope.$emit('recentTrxn');
+				$scope.updateMarketPrice();
+				updatePortfolio();
 			});
-			$rootScope.$emit('bought');
-			$rootScope.$emit('recentTrxn');
+			// $rootScope.$emit('bought');
+			// $rootScope.$emit('recentTrxn');
 			// $scope.updateMarketPrice();
-			 updatePortfolio();
+
 		}
 	};
 
@@ -137,7 +142,6 @@ angular.module('app.portfolio', [])
 		};
 
 	// MY STOCKS MODAL
-	updatePortfolio();
 	// $scope.updateMarketPrice();
 
 	function updatePortfolio(){
@@ -170,4 +174,7 @@ angular.module('app.portfolio', [])
 
     $rootScope.$emit("PortfolioUpdate", {});
 	}
+
+	updatePortfolio();
+
 }]);
