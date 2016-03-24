f61abf7c8b3736acd94e133211f89f193354042e
Devonte M <devontem@live.com>
2016-03-23 17:16:50 -0700

    fixed update stocks if only one symbol is in list

--- server/portfolios/portfolioController.js
+++ server/portfolios/portfolioController.js
@@ -14,6 +14,8 @@ module.exports.getUserStocks = function(req, res){
     Transaction.findAll({ where: {
       PortfolioId: portfolio.id
     }}).then(function(transactions){
+      var originalPrice = transactions[0].price;
+      var originalShares = transactions[0].shares;
 
       // minimizes doubles, adds all shares from same company
       var updatedShares = reduceStocks(transactions);
@@ -24,6 +26,12 @@ module.exports.getUserStocks = function(req, res){
       });
 
       res.send(reducedStocks);
+
+      // transacations[0] was altered in reduceStocks, setting it back to original values
+      transactions[0].update({
+        shares: originalShares,
+        price: originalPrice
+      });
     })
     .catch(function(err){
       res.send("There was an error: ", err);
@@ -49,16 +57,43 @@ module.exports.updateUserStocks = function(req, res){
 
       if (transactions.length < 1){ res.send({error:null}); return }
 
-      portfolio.portfolioValue = 0;
+      var portfolioValue = 0;
+      var originalPrice = transactions[0].price;
+      var originalShares = transactions[0].shares;
+
+      // console.log('transactions', transactions.get())
+      var trans = Array.prototype.slice.call(transactions)
+
+      // minimizes doubles, adds all shares from same company
+      var updatedShares = reduceStocks(transactions);
+
+      console.log('updatedShares', updatedShares)
+
+      // returns only the bought shares
+      var reducedStocks = _.filter(updatedShares, function(transaction){
+        return transaction.buysell && transaction.shares > 0;
+      });
+
+      console.log('reducedStocks', reducedStocks)
 
       // creates a list of all user stocks in order to query
       var stockNames = [];
-      transactions.forEach(function(stock){
+      reducedStocks.forEach(function(stock){
         if(stockNames.indexOf(stock.symbol) < 0){
           stockNames.push(stock.symbol);
         }
       });
+
+      // transacations[0] was altered in reduceStocks, setting it back to original values
+      transactions[0].update({
+        shares: originalShares,
+        price: originalPrice
+      });
+
+      console.log('transactions2', transactions)
+      console.log('trans', trans)
       stockNames = stockNames.join(',');
+      console.log('stocknames', stockNames);
 
       var query = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20%3D%20%27"+ stockNames +"%27&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&format=json";
 
@@ -69,30 +104,56 @@ module.exports.updateUserStocks = function(req, res){
         // Too many queries instantaneously cause API to fail, sends error emit to client
         if (!stocks.body.query){ res.send({error:true}); return 0; }
 
+        // if only one stock is being updated
         if (stocks.body.query.count === 1){
-          transactions[0].marketPrice = stocks.body.query.results.quote.Ask;
-          transactions[0].return = ( (transactions[0].marketPrice - transactions[0].price ) / transactions[0].price) * 100;
-          portfolio.portfolioValue += parseFloat(transactions[0].marketPrice) * transactions[0].shares;
+          console.log('************** DOING ONLY ONE LOOP');
+          var updatedStock = stocks.body.query.results.quote;
+          for (var i = 0; i < transactions.length; i++){
+            if (transactions[i].symbol === updatedStock.symbol){
+              console.log('SYMBOLS MATCH '+transactions[i].symbol, 'was "'+transactions[i].price+'" and is now "'+updatedStock.Ask+'". Share # is '+transactions[i].shares)
+              // transactions[i].marketPrice = updatedStock.Ask;
 
-          transactions[0].save();
+              // updating portfolio value with new market price * shares, for each transaction (incl negatives [aka sold shares])
+              portfolioValue += (updatedStock.Ask * trans[i].shares);
+              console.log('**********portfolioValue ->'+portfolioValue+' amount added ->'+updatedStock.Ask * transactions[i].shares)
 
-        } else {
-          var updatedStocks = stocks.body.query.results.quote;
-          for (var i = 0; i < updatedStocks.length; i++){
-            for (var j = 0; j < transactions.length; j++){
-              if (updatedStocks[i].symbol === transactions[j].symbol){
-                transactions[j].marketPrice = updatedStocks[i].Ask;
-                transactions[j].return = ( (updatedStocks[i].Ask - transactions[j].price) / transactions[j].price) * 100;
-                // console.log('new price->', newStock, 'oldPrice->', transactions[index].price)
-                portfolio.portfolioValue += parseFloat(transactions[j].marketPrice) * transactions[j].shares;
-                transactions[j].save();
-                break;
-              }
+              transactions[i].update({
+                marketPrice: updatedStock.Ask
+              });
+
+              // transactions[i].save();
             }
           }
+          // transactions[0].marketPrice = stocks.body.query.results.quote.Ask;
+          // transactions[0].return = ( (transactions[0].marketPrice - transactions[0].price ) / transactions[0].price) * 100;
+          // portfolio.portfolioValue += parseFloat(transactions[0].marketPrice) * transactions[0].shares;
+
+          // transactions[0].save();
+
+        } else {
+          // console.log('*************DOING THE MULTIPLE LOOP', stocks.body.query.results.quote);
+          // // when multiple stocks are being updated
+          // var updatedStocks = stocks.body.query.results.quote;
+          // for (var i = 0; i < updatedStocks.length; i++){
+          //   console.log('**** first loop working!')
+          //   for (var j = 0; j < transactions.length; j++){
+          //     console.log('**** second loop working!')
+          //     if (updatedStocks[i].symbol === transactions[j].symbol){
+          //       console.log('SYMBOLS MATCH '+transactions[j].symbol, 'was "'+transactions[j].Ask+'" and is now"'+updatedStocks[i].Ask+'".')
+          //       transactions[j].marketPrice = updatedStocks[i].Ask;
+          //       // transactions[j].return = ( (updatedStocks[i].Ask - transactions[j].price) / transactions[j].price) * 100;
+          //       // console.log('new price->', newStock, 'oldPrice->', transactions[index].price)
+          //       portfolio.portfolioValue += parseFloat(transactions[j].marketPrice) * transactions[j].shares;
+          //       transactions[j].save();
+          //       // break;
+          //     }
+          //   }
+          // }
         }
 
-        portfolio.save();
+        portfolio.update({
+          portfolioValue: portfolioValue
+        })
 
        res.send({error:false});
       });
@@ -147,9 +208,11 @@ function reduceStocks(stocks){
       storage[key][0].price = avgPrice / storage[key].length;
       finalArray.push(storage[key][0]);
     } else {
+      var copy = _.extend({}, storage[key][0]);
       finalArray.push(storage[key][0]);
     }
   }
+  console.log('final array', finalArray)
   return finalArray;
 }
 
