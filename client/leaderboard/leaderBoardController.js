f683a9881d979d302f187d8ffa91f012084d5fb8
abc810221 <abc810221@gmail.com>
2016-03-24 11:33:32 -0700

    rebase

--- client/leaderboard/leaderBoardController.js
+++ client/leaderboard/leaderBoardController.js
@@ -38,6 +38,7 @@ app.controller('LeaderBoardController', ['$scope', '$window', '$stateParams', 'D
           if(portfolios[i].UserId === Number(userId)) joined = true;
           console.log(portfolios[i].UserId, Number(userId) )
         }
+        console.log('********' , joined)
         $scope.portfolios = portfolios;
         $scope.leagueName = portfolios[0].leaguename;
         $scope.code = portfolios[0].code;
