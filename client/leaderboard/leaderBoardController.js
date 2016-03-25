fb9afb5703ce2b9a2d4340ac55eeab7e9cfdf128
jordmur <jordanmurphy11@gmail.com>
2016-03-25 16:50:23 -0500

    (fix) Removes unused code

--- client/leaderboard/leaderBoardController.js
+++ client/leaderboard/leaderBoardController.js
@@ -1,29 +1,5 @@
 app.controller('LeaderBoardController', ['$scope', '$window', '$stateParams', 'DashboardFactory', 'leaderBoardFactory', '$location', '$rootScope' ,function($scope, $window, $stateParams, DashboardFactory, leaderBoardFactory, $location, $rootScope){
 
-  // members will be an object of each member in the league
-  // containing name, portfolio value, and other stats
-  // desired to go on the leaderboard
-  $scope.members = [
-    {
-      username: 'Sonny',
-      value: 15000,
-      return: '10%',
-      transactions: 25
-    },
-    {
-      username: 'Ted',
-      value: 9000,
-      return: '20%',
-      transactions: 45
-    },
-    {
-      username: 'Devonte',
-      value: 3567,
-      return: '6%',
-      transactions: 5
-    }
-  ];
-
   $scope.leagueId = $stateParams.leagueId;
   $scope.portfolios;
   $scope.leagueName;
