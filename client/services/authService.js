b1bdcc9e650646a046970586f828c440643a6af0
abc810221 <abc810221@gmail.com>
2016-03-24 22:37:13 -0700

    feat/Email user temp password when they forget

--- client/services/authService.js
+++ client/services/authService.js
@@ -20,7 +20,21 @@ app.factory('Auth', function($http, $location, $window){
     .then(function(data){
       return data.data;
     })
-  }
+  };
+
+  var forgotpw = function(email){
+    return $http({
+      method: 'POST',
+      url: '/api/users/forgotpw',
+      data: {
+          email: email
+      }
+    })
+    .then(function(data){
+      console.log("data", data)
+      return data.data;
+    })
+  };
 
   var isAuth = function() {
     return !!$window.localStorage.getItem('com.tp');
@@ -29,6 +43,7 @@ app.factory('Auth', function($http, $location, $window){
   return {
     isAuth: isAuth,
     createuser: createuser,
+    forgotpw: forgotpw,
     loginuser: loginuser
   };
 
