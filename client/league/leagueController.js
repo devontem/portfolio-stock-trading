ce91f71398c88ae78436b4bef3401add8a2fb288
Devonte M <devontem@live.com>
2016-03-25 12:10:11 -0700

    testing invite league

--- client/league/leagueController.js
+++ client/league/leagueController.js
@@ -48,5 +48,10 @@ app.controller('LeagueController', ['$scope', '$stateParams', 'DashboardFactory'
       }
     });
 
+     $(document).ready(function(){
+      // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+      $('.modal-trigger').leanModal();
+    });
+      
 
 }]);
